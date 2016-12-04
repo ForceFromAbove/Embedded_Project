8ccb968dbae4a303126b97d7912420359a6ca871
Aaron Ewing <aaronewing@nnu.edu>
2016-12-03 20:35:29 -0700

    cant think of anything else to do

--- Sensor_Test/sensor_Read.c
+++ Sensor_Test/sensor_Read.c
@@ -5,8 +5,14 @@
  *      Author: AaronEwing
  */
 
+
+
 #include <msp430.h>
 #include <stdint.h>
+#include <stdbool.h>
+
+bool OMGsomethingworksflag = 0;
+
 
 	uint16_t temp_Conversion (raw_Temp) {
 	uint32_t hold_Value = 0;
@@ -42,18 +48,84 @@ uint16_t read_Sensor (uint8_t slaveAddress, uint8_t registerAddress) {
 	uint8_t dataLSB = 0x00;
 	uint16_t raw_Temp;
 	uint16_t temperature = 0;
+	uint16_t i = 0x00;		// funky things at 0xC4=i / 0x9A=j
+	uint16_t j = 0x00;
 
 
+for (i = 0x00; i <= 0xFF; i++ ) {
+	for (j = 0x00; j <= 0xFF; j++ ) {
 	while (UCB1CTL1 & UCTXSTP);               				// Ensure stop condition got sent
 
-	UCB1CTL1 |= UCTR + UCTXSTT;
-	// START
+	UCB1CTL1 |= UCTR + UCTXSTT;								// START
 	while ((UCB1CTL1 & UCTXSTT) || !(UCB1IFG & UCTXIFG0));
-	UCB1TXBUF = slaveAddress;								// SLAVE ADDRESS
+	//UCB1TXBUF = slaveAddress;								// SLAVE ADDRESS
+
+	UCB1TXBUF = i;
 	while (!(UCB1IFG & UCTXIFG0));
-	UCB1TXBUF = registerAddress;							// MEASURE COMMAND
+	//UCB1TXBUF = registerAddress;							// MEASURE COMMAND
+	UCB1TXBUF = j;
+
+	if (i > 0xC0) {
+			__delay_cycles(100000);
+		}
+	__delay_cycles(500);
+
 	while (!(UCB1IFG & UCTXIFG0));
-	UCB1TXBUF = slaveAddress;								// SLAVE ADDRESS
+	//UCB1TXBUF = slaveAddress;								// SLAVE ADDRESS
+	UCB1TXBUF = i;
+
+
+	if (i > 0xC0) {
+		__delay_cycles(100000);
+	}
+	__delay_cycles(10000);
+	}
+}
+
+/*
+	for (i = 0x00; i <= 0xFF; i++ ) {
+		for (j = 0x00; j <= 0xFF; j++ ) {
+		while (UCB1CTL1 & UCTXSTP);               				// Ensure stop condition got sent
+
+		UCB1CTL1 |= UCTR + UCTXSTT;								// START
+		while ((UCB1CTL1 & UCTXSTT) || !(UCB1IFG & UCTXIFG0));
+		//UCB1TXBUF = slaveAddress;								// SLAVE ADDRESS
+
+		UCB1TXBUF = i;
+		while (!(UCB1IFG & UCTXIFG0));
+		//UCB1TXBUF = registerAddress;							// MEASURE COMMAND
+		UCB1TXBUF = j;
+
+		if (i > 0xC0) {
+				__delay_cycles(100000);
+			}
+		__delay_cycles(500);
+
+		while (!(UCB1IFG & UCTXIFG0));
+		//UCB1TXBUF = slaveAddress;								// SLAVE ADDRESS
+		UCB1TXBUF = i;
+
+
+		if (i > 0xC0) {
+			__delay_cycles(100000);
+		}
+		__delay_cycles(10000);
+
+		while (!(UCB1IFG & UCTXIFG0));
+		//UCB1TXBUF = slaveAddress;								// SLAVE ADDRESS
+		UCB1TXBUF = i;
+
+		if (i > 0xC0) {
+			__delay_cycles(100000);
+		}
+		__delay_cycles(1000000);
+
+		while (!(UCB1IFG & UCTXIFG0));
+		//UCB1TXBUF = slaveAddress;								// SLAVE ADDRESS
+		UCB1TXBUF = i;
+		}
+	}
+	*/
 
 	while (!(UCB1IFG & UCRXIFG0));
 	dataMSB = UCB1RXBUF;
@@ -69,3 +141,33 @@ uint16_t read_Sensor (uint8_t slaveAddress, uint8_t registerAddress) {
 	return temperature;
 }
 
+#pragma vector = USCI_B1_VECTOR
+__interrupt void USCI_B1_ISR(void) {
+
+	OMGsomethingworksflag = 1;
+
+  switch(__even_in_range(UCB1IV, USCI_I2C_UCBIT9IFG)) {
+    case USCI_NONE:          break;         // Vector 0: No interrupts
+    case USCI_I2C_UCALIFG:   break;         // Vector 2: ALIFG
+    case USCI_I2C_UCNACKIFG: break;         // Vector 4: NACKIFG
+    case USCI_I2C_UCSTTIFG:  break;         // Vector 6: STTIFG
+    case USCI_I2C_UCSTPIFG:                 // Vector 8: STPIFG
+    //  TXData = 0;
+      UCB0IFG &= ~UCSTPIFG;                 // Clear stop condition int flag
+      break;
+    case USCI_I2C_UCRXIFG3:  break;         // Vector 10: RXIFG3
+    case USCI_I2C_UCTXIFG3:  break;         // Vector 12: TXIFG3
+    case USCI_I2C_UCRXIFG2:  break;         // Vector 14: RXIFG2
+    case USCI_I2C_UCTXIFG2:  break;         // Vector 16: TXIFG2
+    case USCI_I2C_UCRXIFG1:  break;         // Vector 18: RXIFG1
+    case USCI_I2C_UCTXIFG1:  break;         // Vector 20: TXIFG1
+    case USCI_I2C_UCRXIFG0:  break;         // Vector 22: RXIFG0
+    case USCI_I2C_UCTXIFG0:                 // Vector 24: TXIFG0
+ //     UCB0TXBUF = TXData++;
+      break;
+    case USCI_I2C_UCBCNTIFG: break;         // Vector 26: BCNTIFG
+    case USCI_I2C_UCCLTOIFG: break;         // Vector 28: clock low timeout
+    case USCI_I2C_UCBIT9IFG: break;         // Vector 30: 9th bit
+    default: break;
+  }
+}
